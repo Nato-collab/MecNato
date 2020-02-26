3bfc5bb024d9ddadc786545fe9d79adc38440b1e
Nato <49217848+Guaigon@users.noreply.github.com>
2020-02-26 13:44:02 -0600

    Mec

---
+++ Assets/Scenes/Cinetica y potencial/Scripts/Ball.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Ball : MonoBehaviour
+{
+    public float thrust = 10.0f;
+    float k;
+    float x = 10;
+    float ep;
+    float ec;
+    float m = 3;
+    float v;
+    float rotY, rotX;
+    Quaternion rota;
+    public Rigidbody rig;
+    public Text valores;
+
+    void EnergiaPotElastica()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rotY++;
+            rota = Quaternion.Euler(-rotY, 0, 0);
+            rig.rotation = rota;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rotX++;
+            rota = Quaternion.Euler(0, -rotX, 0);
+            rig.rotation = rota;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rotX++;
+            rota = Quaternion.Euler(0, rotX, 0);
+            rig.rotation = rota;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            rotY++;
+            rota = Quaternion.Euler(rotY, 0, 0);
+            rig.rotation = rota;
+        }
+        if (Input.GetKey(KeyCode.K))
+        {
+            k += 1;
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            k = 0;
+            ep = 0;
+            ec = 0;
+            v = 0;
+            rotY = 0;
+            rotX = 0;
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            rig.AddForce(transform.forward * v * thrust);
+
+
+        }
+
+        ep = x * x * k;
+        ec = ep;
+        v = ec * 2 / m;
+
+        valores.text = ("Ep = " + ep + "J" + " Ec = " + ec + "J" + "   x = " + x + "m" + "   v = " + v + "m/s" + "   k = " + k + "N/m" + "   m = " + m + "kg");
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        EnergiaPotElastica();
+    }
+}
---
+++ Assets/Scenes/Practica 1/Scripts/Movimientovectorialyescalar.cs
@@ -0,0 +1,189 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Movimientovectorialyescalar : MonoBehaviour
+{
+
+    public Text escalar;
+
+    public Text vectorial;
+
+    public float velocidadCubo;
+
+    public float rotacionCubo;
+
+    public Transform[] posiciones;
+
+    public float metrosX;
+
+    public float metrosZ;
+
+    float totalMetros;
+
+    bool norte;
+    bool sur;
+    bool este;
+    bool oeste;
+
+    void Start()
+    {
+        posiciones[0].position = transform.position;
+        escalar.text = ("Te moviste 0 metros");
+        vectorial.text = ("Te moviste 0 metros");
+    }
+
+    void Update()
+    {
+
+        //ME DISCULPO POR USAR CODIGO DE FUERZA BRUTA
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(Vector3.forward * velocidadCubo * Time.deltaTime, Space.Self);
+
+            if (posiciones[0].position.x < transform.position.x)
+            {
+                metrosX = transform.position.x - posiciones[0].position.x;
+            }
+            else if (posiciones[0].position.x > transform.position.x)
+            {
+                metrosX = posiciones[0].position.x - transform.position.x;
+            }
+            else
+            {
+
+            }
+
+            if (posiciones[0].position.z < transform.position.z)
+            {
+                metrosZ = transform.position.z - posiciones[0].position.z;
+            }
+            else if (posiciones[0].position.z > transform.position.z)
+            {
+                metrosZ = posiciones[0].position.z - transform.position.z;
+            }
+            else
+            {
+
+            }
+
+            totalMetros = metrosX + metrosZ;
+
+            escalar.text = ("Te moviste " + totalMetros + " metros");
+            if (posiciones[0].position.x < transform.position.x)
+            {
+                if (posiciones[0].position.z < transform.position.z)
+                {
+                    vectorial.text = ("Te moviste " + totalMetros + " metros al noreste");
+                }
+                else if (posiciones[0].position.z > transform.position.z)
+                {
+                    vectorial.text = ("Te moviste " + totalMetros + " metros al noroeste");
+                }
+                else
+                {
+                    vectorial.text = ("Te moviste " + totalMetros + " metros al norte");
+                }
+            }
+            else
+            {
+                if (posiciones[0].position.z < transform.position.z)
+                {
+                    vectorial.text = ("Te moviste " + totalMetros + " metros al sureste");
+                }
+                else if (posiciones[0].position.z > transform.position.z)
+                {
+                    vectorial.text = ("Te moviste " + totalMetros + " metros al suroeste");
+                }
+                else
+                {
+                    vectorial.text = ("Te moviste " + totalMetros + " metros al sur");
+                }
+            }
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+           
+            transform.Translate(-Vector3.forward * velocidadCubo * Time.deltaTime, Space.Self);
+            if (posiciones[0].position.x < transform.position.x)
+            {
+                metrosX = transform.position.x - posiciones[0].position.x;
+            }
+            else if (posiciones[0].position.x > transform.position.x)
+            {
+                metrosX = posiciones[0].position.x - transform.position.x;
+            }
+            else
+            {
+
+            }
+
+            if (posiciones[0].position.z < transform.position.z)
+            {
+                metrosZ = transform.position.z - posiciones[0].position.z;
+            }
+            else if (posiciones[0].position.z > transform.position.z)
+            {
+                metrosZ = posiciones[0].position.z - transform.position.z;
+            }
+            else
+            {
+
+            }
+
+            totalMetros = metrosX + metrosZ;
+
+            escalar.text = ("Te moviste " + totalMetros + " metros");
+
+            if (posiciones[0].position.x < transform.position.x)
+            {
+                if (posiciones[0].position.z < transform.position.z)
+                {
+                    vectorial.text = ("Te moviste " + totalMetros + " metros al noreste");
+                }
+                else if (posiciones[0].position.z > transform.position.z)
+                {
+                    vectorial.text = ("Te moviste " + totalMetros + " metros al noroeste");
+                }
+                else
+                {
+                    vectorial.text = ("Te moviste " + totalMetros + " metros al norte");
+                }
+            }
+            else
+            {
+                if (posiciones[0].position.z < transform.position.z)
+                {
+                    vectorial.text = ("Te moviste " + totalMetros + " metros al sureste");
+                }
+                else if (posiciones[0].position.z > transform.position.z)
+                {
+                    vectorial.text = ("Te moviste " + totalMetros + " metros al suroeste");
+                }
+                else
+                {
+                    vectorial.text = ("Te moviste " + totalMetros + " metros al sur");
+                }
+            }
+
+            
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(Vector3.up, rotacionCubo * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(Vector3.up, -rotacionCubo * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            posiciones[0].position = transform.position;
+            escalar.text = ("Te moviste 0 metros");
+            vectorial.text = ("Te moviste 0 metros");
+        }
+    }
+
+}
