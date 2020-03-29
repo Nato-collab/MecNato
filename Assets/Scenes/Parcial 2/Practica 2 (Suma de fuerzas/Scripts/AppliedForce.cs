501ac0d16e9500a2b660a73e23cf44cde872c0d3
Nato <49217848+Guaigon@users.noreply.github.com>
2020-03-29 03:20:43 -0600

    Practica 2 y empiezo practica 3

---
+++ Assets/Scenes/Parcial 2/Practica 2 (Suma de fuerzas/Scripts/AppliedForce.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AppliedForce : MonoBehaviour
+{
+    Vector3 newPos = new Vector3(0, 0, 0);
+    public Text robotTeam;
+    public Text ninjaTeam;
+    public int robotTeamN = 0;
+    public int ninjaTeamN = 0;
+
+    void Start()
+    {
+        newPos = gameObject.transform.position;
+        ninjaTeam.text = ("Ninja team force: " + ninjaTeamN + "N");
+        robotTeam.text = ("Robot team force: " + robotTeamN + "N");
+    }
+
+    public void OnClickRight()
+    {
+        ninjaTeamN++;
+        newPos.x += .2f;
+        gameObject.transform.position = newPos;
+        ninjaTeam.text = ("Ninja team force: " + ninjaTeamN + "N");
+    }
+
+    public void OnClickLeft()
+    {
+        robotTeamN++;
+        newPos.x -= .2f;
+        gameObject.transform.position = newPos;
+        robotTeam.text = ("Robot team force: " + robotTeamN + "N");
+    }
+}
